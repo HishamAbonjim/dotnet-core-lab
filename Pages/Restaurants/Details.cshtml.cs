c8b53fc8cae59d4b6a22138c3e2d2423c2ad2d18
hishamabonjim <hisham.abonjim@gmail.com>
2019-10-05 00:59:32 +0300

    80%

---
+++ Pages/Restaurants/Details.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using OdeToFood.Core;
+using OdeToFood.Data;
+
+namespace OdeToFood.Pages.Restaurants
+{
+    public class DetailsModel : PageModel
+    {
+        private IRestaurantData restaurantData;
+
+        public Restaurant Restaurant { get; set; }
+        public DetailsModel(IRestaurantData restaurantData)
+        {
+            this.restaurantData = restaurantData; 
+        }
+        public IActionResult OnGet(int Id)
+        {
+            Restaurant = restaurantData.GetById(Id);
+            if (Restaurant == null)
+                return RedirectToPage("../Shared/NotFound");
+            return Page();
+        }
+    }
+}
---
+++ Pages/Restaurants/Edit.cshtml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using OdeToFood.Core;
+using OdeToFood.Data;
+
+namespace OdeToFood.Pages.Restaurants
+{
+    public class EditModel : PageModel
+    {
+        private  IRestaurantData restaurantData;
+        private IHtmlHelper htmlHelper;
+        [BindProperty]
+        public Restaurant Restaurant { get; set; }
+        public IEnumerable<SelectListItem> Cusinis   { get; set; }
+        public EditModel(IRestaurantData restaurantData , IHtmlHelper htmlHelper)
+        {
+            this.restaurantData = restaurantData;
+            this.htmlHelper = htmlHelper; 
+        }
+        public IActionResult OnGet(int?  Id)
+        {
+            Cusinis = htmlHelper.GetEnumSelectList<CuisineType>();
+            if (Id.HasValue)
+                Restaurant = restaurantData.GetById(Id.Value);
+            else
+                Restaurant = new Restaurant();
+            if (Restaurant == null)
+                return RedirectToPage("../Shared/NotFound");
+            return Page();
+        }
+        public IActionResult OnPost()
+        {
+                Cusinis = htmlHelper.GetEnumSelectList<CuisineType>();
+            if (ModelState.IsValid)
+            {
+                if (Restaurant.Id == 0)
+                {
+                    restaurantData.Add(Restaurant); 
+
+                }
+                else
+                {
+                    restaurantData.Update(Restaurant);
+                }
+                    restaurantData.Commit();
+            return RedirectToPage("./Details", new {Id=Restaurant.Id});
+            }
+            return Page(); 
+        }
+    }
+}
